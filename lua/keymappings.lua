f47b2d52b74ce03e917e49d394737c3c6d81c7dd
SonglinBai <1353011752@qq.com>
2021-07-10 22:30:52 +0800

    nothing

--- lua/keymappings.lua
+++ lua/keymappings.lua
@@ -91,8 +91,8 @@ vim.api.nvim_set_keymap(
 
 -- Telescope
 vim.api.nvim_set_keymap("n", "<leader>ff", ":Telescope find_files<CR>", {noremap = true})
--- vim.api.nvim_set_keymap("n", "<leader>fg", ":Telescope live_grep<CR>", { noremap = true })
-vim.api.nvim_set_keymap("n", "<leader>fg", ":Telescope grep_string<CR>", {noremap = true})
+vim.api.nvim_set_keymap("n", "<leader>fg", ":Telescope live_grep<CR>", { noremap = true })
+-- vim.api.nvim_set_keymap("n", "<leader>fg", ":Telescope grep_string<CR>", {noremap = true})
 vim.api.nvim_set_keymap("n", "<leader>fb", ":Telescope buffers<CR>", {noremap = true})
 vim.api.nvim_set_keymap("n", "<leader>fh", ":Telescope help_tags<CR>", {noremap = true})
 vim.api.nvim_set_keymap("n", "<leader>fj", ":Telescope jumplist<CR>", {noremap = true})
