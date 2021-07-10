4a5a7575e82f38672e4ebe24c66222fe6226d6a3
SonglinBai <1353011752@qq.com>
2021-07-10 22:25:21 +0800

    fix toggle term

--- lua/config/toggleterm.lua
+++ lua/config/toggleterm.lua
@@ -7,17 +7,17 @@ require("toggleterm").setup{
       return vim.o.columns * 0.4
     end
   end,
-  open_mapping = [[<c-\>]],
+  open_mapping = [[<A-;>]],
   hide_numbers = true, -- hide the number column in toggleterm buffers
   shade_filetypes = {},
   shade_terminals = true,
   shading_factor = '1', -- the degree by which to darken to terminal colour, default: 1 for dark backgrounds, 3 for light
   start_in_insert = true,
-  insert_mappings = false, -- whether or not the open mapping applies in insert mode
+  insert_mappings = true, -- whether or not the open mapping applies in insert mode
   persist_size = true,
   direction = 'horizontal',
   close_on_exit = true, -- close the terminal window when the process exits
-  shell = vim.fn.has('win32') == 1 and "pwsh" or "zsh"
+  shell = vim.fn.has('win32') == 1 and "cmd" or "zsh"
 }
 
 local Terminal  = require('toggleterm.terminal').Terminal
