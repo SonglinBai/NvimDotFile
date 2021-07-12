ceeed6fc427cf951a92e0ebba7579d8ce2b6296e
SonglinBai <1353011752@qq.com>
2021-07-13 01:42:54 +0800

    add tasks.ini

--- lua/config/asynctasks.lua
+++ lua/config/asynctasks.lua
@@ -1,3 +1,11 @@
 local g = vim.g
 
 g.asyncrun_open = 6
+
+if vim.fn.has('win32') == 1 then
+  g.asynctasks_extra_config = {'~/AppData/Local/nvim/tasks.ini'}
+else
+  g.asynctasks_extra_config = {'~/.config/nvim/tasks.ini'}
+end
+
+g.asynctasks_term_pos = 'external'
